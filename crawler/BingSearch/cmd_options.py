a15e376013b23f1e113fad578fb0d252f39e248f
matasukef <f-e@toki.waseda.jp>
2017-03-17 22:58:52 +0900

    fix it

--- crawler/BingSearch/cmd_options.py
+++ crawler/BingSearch/cmd_options.py
@@ -1,5 +1,8 @@
 import argparse
 
+#The number of images bing search API allows you to download per one request:
+LIMIT = 150
+
 def get_arguments():
 
     languages = ['es-AR','en-AU','de-AT','nl-BE','fr-BE','pt-BR','en-CA','fr-CA','es-CL','da-DK','fi-FI','fr-FR','de-DE','zh-HK','en-IN','en-ID','en-IE','it-IT','ja-JP','ko-KR','en-MY','es-MX','nl-NL','en-NZ','no-NO','zh-CN','pl-PL','pt-PT','en-PH','ru-RU','ar-SA','en-ZA','es-ES','sv-SE','fr-CH','de-CH','zh-TW','tr-TR','en-GB','en-US','es-US']
@@ -25,6 +28,9 @@ def get_arguments():
     parser.add_argument(
             '-f', '--saveFile', type=str, default="download_list.csv",
             help="file name for saving list of image urls")
+    parser.add_argument(
+            '-i', '--iterate', type=int, default=1,
+            help="The number of iteration you want to sent requests")
 
     args = parser.parse_args()
 

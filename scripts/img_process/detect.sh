7649b9e9e8bd38ae5999fa32acbefafd0de3ccad
matasukef <f-e@toki.waseda.jp>
2017-06-07 23:17:38 +0900

    fix it

---
+++ scripts/img_process/detect.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ ! -e $1 && ! -e $2]; then
+    echo "USAGE: bash $CMDNAE [source dir] [target dir]" 1>$2
+    exit 1
+fi
+
+
+SOURCE_PATH=$1
+TARGET_PATH=$2
+
+for DIRPATH in ${SOURCE_PATH}*
+do
+    python process.py ${DIRPATH} ${TARGET_PATH}
+done
+
